8a58fff91dd3050e3a889c0162c0f6cef295c90f
Joel Thomas <joel.thomas@quolab.com>
2021-02-20 22:44:50 +0100

    Fixed output

--- scripts/claim.js
+++ scripts/claim.js
@@ -2,23 +2,18 @@ const InheritFarm = artifacts.require('InheritFarm')
 module.exports = async function(callback) {
 	//var accounts;
 	//web3.eth.getAccounts(function(err,res) { accounts = res; });
-	console.log('1')
-
 	let benefactor = "0x76716b712743F763946c64aF9a471b2707A80c49"
 	let heir = "0xF4cD6098283f8848eAC6223ECBee4148B0d5B546"
 
-	console.log('2')
+	console.log('Instantiating InheritFarm')
 	let ihf = await InheritFarm.deployed()
-	console.log('3')
 	let now = Math.floor(Date.now() / 1000)
-	console.log('4')
+	console.log('Checking expiration (diff in s)')
 	let expires_in = (await ihf.expirations(benefactor)).toNumber()
-	console.log('5')
-	console.log('expiration: ' + expires_in)
-	console.log('now       : ' + now)
-	console.log('Expires in: ' + (expires_in - now) + "s")
+	console.log('  Expires in: ' + (expires_in - now) + "s")
 
+	console.log('Claiming inheritance')
 	await ihf.claim(benefactor, {from: heir})
-	console.log('success')
+	console.log('Success')
 	callback()
 }
