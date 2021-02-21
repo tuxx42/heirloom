c52b7b55f40ac6e8c131fb216be346ddc76e02ac
Joel Thomas <joel.thomas@quolab.com>
2021-02-21 12:56:56 +0100

    Add changes

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -12,11 +12,11 @@ module.exports = async function (deployer, network, accounts) {
 
   //console.log("transfering DAI to benefactor")
   //await daiToken.transfer(accounts[0], 1500000000)
-  console.log("approving DAI from benefactor")
+  console.log("Approving DAI from benefactor")
   await daiToken.approve(InheritFarm.address, "1500000000", {from: accounts[0]})
-  console.log("inheiriting DAI")
+  console.log("Inheiriting DAI")
   let timevar = Math.floor(Date.now() / 1000) + 60
   let success = await inheritFarm.inherit(accounts[1],   "1400000000", timevar)
   console.log(success)
-  console.log("success")
+  console.log("Success")
 };
